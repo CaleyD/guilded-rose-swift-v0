88679f50ce8613f0efc8738e0edead5b20ba0790
Davis, Caley <caldavis@ebay.com>
2018-01-10 11:31:50 -0800

    refactoring Kata

---
+++ GildedRose/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  GildedRose
+//
+
+public struct Item: Equatable {
+    
+    var name: String
+    var sellIn: Int
+    var quality: Int
+    
+}
+
+public func ==(lhs: Item, rhs: Item) -> Bool {
+    return lhs.name == rhs.name && lhs.sellIn == rhs.sellIn && lhs.quality == rhs.quality
+}
